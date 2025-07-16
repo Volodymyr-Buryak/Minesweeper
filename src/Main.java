bbbcc1c30f6b02a1f5527975b06115dd1113d6b7
Vova <space24@gmail.com>
2025-07-16 17:29:20 +0300

    Commit Demo4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+
+        try (Scanner reader = new Scanner(System.in)) {
+            // Read input from the user
+            System.out.print("Enter a number: ");
+            int number = reader.nextInt();
+
+            // Print the number entered by the user
+            System.out.println("You entered: " + number);
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+
+        }
+
     }
 }
