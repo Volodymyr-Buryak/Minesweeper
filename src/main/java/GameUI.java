ca1f8624b08ad2a3ea1c4c3b8595fbf9baf05a25
Vova <space24@gmail.com>
2025-07-17 19:51:12 +0300

    Checked for the presence of the file in the resources.

--- src/main/java/GameUI.java
+++ src/main/java/GameUI.java
@@ -1,7 +1,16 @@
 package main.java;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+import java.awt.Image;
+import java.awt.Graphics;
+import java.awt.Dimension;
+
+import java.io.FileNotFoundException;
 import java.net.URL;
 
 public class GameUI extends JFrame {
@@ -25,11 +34,21 @@ public class GameUI extends JFrame {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                g.drawImage(getImage("bomb.png"), 0,0, this);
+                try {
+                    g.drawImage(getImage("sау.png"), 0,0, this);
+                } catch (FileNotFoundException e) {
+                    System.out.println("Error loading image: " + e.getMessage());
+                    showErrorAndExit(e.getMessage());
+                }
             }
         };
         panel.setPreferredSize(new Dimension(500, 300));
         add(panel);
+    }
+
+    private void showErrorAndExit (String message) {
+        JOptionPane.showMessageDialog(this, message, "Error loading resource", JOptionPane.ERROR_MESSAGE);
+        System.exit(1);
     }
 
     private  void initializeJFrame() {
@@ -41,18 +60,16 @@ public class GameUI extends JFrame {
         setVisible(true);
     }
 
-    private Image getImage (String iconName) {
-
+    private Image getImage (String iconName) throws FileNotFoundException {
         if (iconName == null || iconName.isBlank() || !iconName.endsWith(".png")){
             throw new IllegalArgumentException("Icon name cannot be null, blank, or end with .png");
         }
 
         URL imgURL = getClass().getResource("/img/" + iconName.toLowerCase().strip());
-
-        if (imgURL != null) {
-            return new ImageIcon(imgURL).getImage();
+        if (imgURL == null){
+            throw new FileNotFoundException("Image not found: " + iconName);
         }
 
-        throw new IllegalArgumentException("Image not found: " + iconName);
+        return new ImageIcon(imgURL).getImage();
     }
 }
